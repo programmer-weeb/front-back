a03f8f53097d1d718e230ab632d705d073f0250d
AhmedWindows11 <forprogramming8@gmail.com>
2023-05-13 21:21:15 +0300

    remove some comments

--- back/server.js
+++ back/server.js
@@ -3,9 +3,6 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = 5000;
 const path = require('path');
-
-// const fs = require('fs');
-// const __dirname = path.resolve();
 const cors = require("cors");
 
 let globalId = 3;
@@ -41,16 +38,11 @@ const persons = [
     
 ]
 
-// app.use(express.static('front'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'index.html'))
-    // res.sendFile(path.join(__dirname, 'index.html'))
-    // res.sendFile(path.join(__dirname, 'front', 'index.html'))
-    // res.sendFile('/front/index.html')
     res.json(persons)
 })
 
